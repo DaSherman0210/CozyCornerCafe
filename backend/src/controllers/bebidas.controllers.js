96a0297a8a6b65b5a19ead31e634081fec305e97
DaSherman0210 <tgerman181@gmail.com>
2023-07-29 12:57:01 -0500

    feat: :sparkles: Controllers bebidas
    Did the controllers "bebidas" all the CRUD

---
+++ backend/src/controllers/bebidas.controllers.js
@@ -0,0 +1,79 @@
+import bebidas from "../models/Bebidas.js"
+
+const getBebidas = async (req,res) =>{
+    try {
+        const bebida = await bebidas.find();
+        res.json(bebida);
+    } catch (error) {
+        console.log(error);
+    }
+};  
+
+const getBebida = async (req,res) =>{
+    try {
+        const bebida = await bebidas.findOne({_id:req.params.id})
+        res.json(bebida);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const postBebidas = async (req,res) =>{
+    try {
+        const {nombre,tipo,tipoEspecifico,precio,descripcion,imagen} = req.body;
+        const bebida = new bebidas({nombre,tipo,tipoEspecifico,precio,descripcion,imagen});
+
+        //? Validacion por nombre
+
+        const existeBebida = await bebidas.findOne({_id:req.body.id})
+        if (existeBebida) {
+            return res.status(400).json({
+                msg: "Ya esta registrada esta bebida"
+            });
+        }
+
+        const nuevaBebida = await bebida.save();
+        res.json(nuevaBebida);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const deleteBebida = async (req,res) =>{
+    try {
+        await bebidas.deleteOne({_id:req.body.id});
+        res.status(204).send();
+    } catch (error) {
+        console.log(error);
+    }
+} 
+
+const updateCiclista = async (req,res) =>{
+    try {
+        const bebida = await bebidas.findOne({_id:req.body.id});
+        if (req.body.nombre) {
+            bebida.nombre = req.body.nombre; 
+        }
+        if (req.body.tipo) {
+            bebida.tipo = req.body.tipo; 
+        }
+        if (req.body.tipoEspecifico) {
+            bebida.tipoEspecifico = req.body.tipoEspecifico; 
+        }
+        if (req.body.precio) {
+            bebida.precio = req.body.precio; 
+        }
+        if (req.body.descripcion) {
+            bebida.descripcion = req.body.descripcion; 
+        }
+        if (req.body.imagen) {
+            bebida.imagen = req.body.imagen; 
+        }
+        await bebida.save();
+        res.json(bebida);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export {getBebidas,getBebida,postBebidas,deleteBebida,updateCiclista};
