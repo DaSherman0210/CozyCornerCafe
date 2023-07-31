081dc2c1f5ed154b7ac74ffb006f77519576ad9b
DaSherman0210 <tgerman181@gmail.com>
2023-07-31 13:48:27 -0500

    feat: :safety_vest: validation
    Factura validation

--- backend/src/routes/facturas.routes.js
+++ backend/src/routes/facturas.routes.js
@@ -16,7 +16,16 @@ router.post("/add",[
         for (let index = 0; index < pedidos.length; index++) {
             const existeBebida = await bebidas.findOne({ nombre: pedidos[index].nombre });
             if (!existeBebida) {
-                throw new Error(`La bebida  y tampoco  no esta en la base de datos`)
+                throw new Error("La/s bebida/s no esta/n en la base de datos");
+            }
+            const topping = pedidos[index].toppings
+            console.log(topping);
+            for (let i = 0; i < topping.length; i++) {
+                const existeTopping = await toppings.findOne({nombre: pedidos[index].toppings[index].nombre});
+                if (!existeTopping) {
+                    throw new Error('Saldra esta validacion?');
+                }
+
             }
         }
     }),
